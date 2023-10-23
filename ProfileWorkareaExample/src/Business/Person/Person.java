f150273bc893f8d2832c4cc3c1d14564f6690f0f
Nikesh984 <biraggari.n@northeastern.edu>
2023-10-23 16:34:26 -0400

    Initial_Commit(Updated Person Class)

---
+++ ProfileWorkareaExample/src/Business/Person/Person.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Person;
+
+/**
+ *
+ * @author kal bugrara
+ */
+public class Person {
+
+    String id;
+    
+    String firstName;
+    
+    String lastName;
+    
+    String email;
+    
+    String ssn;
+    
+    String passportNumber;
+    
+    
+     public Person(String id, String firstName, String lastName, String email) {
+
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
+    public void setPassportNumber(String passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+    
+    
+    public String getPersonId() {
+        return id;
+    }
+
+    public boolean isMatch(String id) {
+        if (getPersonId().equals(id)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return getPersonId();
+    }
+}
